e330d2ecc92a2a2c561d43b0703acfab9c76fd5f
EvaFro <frolli.erin@gmail.com>
2018-02-12 12:26:49 -0800

    Added psudocode to plan out my app

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,25 @@
+// Goals of this file:
+//1: Create functionallity of a tic tac toe game
+//2: Allow players to click with in side the board to place game pieces
+//3: Create a popup that lets the users know who won or tie.
+//4: Have a button to clear the board when a game is done.
+
+// I: user clicks on page with mouse
+// O: X/O peices get placed on the board
+	// at end of game popup for winner/tie
+
+//E: if there i a piece that is placed and user clicks on that spot
+	// pop up that says invaled move
+//Note: start with X piece and alternate players -  
+	// once a player clicks on the board can't go back
+
+
+// Helper Functions:
+// * lisen4Click
+// * placePeiceOnBoard
+// * searchWinner
+// * declareWinner
+// * clearBoard
+
 
 
