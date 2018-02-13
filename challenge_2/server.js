9acac65012be24e56d5e1adb134d9540c2b9ae4c
EvaFro <frolli.erin@gmail.com>
2018-02-13 10:40:13 -0800

    Added psudocode for functions I want to create to convert Obj to csv

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
 
 
+
+
+// Create a flatten function
+	// takes children and maps them into an array 
+// Create a makeCSV function
+	// takes the flattenArray
+		// converts to a csv file 
+			// all colmn data except children
+			// all data in same order as original obj
+			// all comma dilimiated
+
+
