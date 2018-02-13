1dc980936382ed471015d857b0e0a3c012581aed
EvaFro <frolli.erin@gmail.com>
2018-02-13 12:29:56 -0800

    uncomented functions

--- challenge_2/convertCSV.js
+++ challenge_2/convertCSV.js
@@ -2,67 +2,67 @@
 
 
 
-// // Create a flatten function
-// // Goal: takes children and maps them into an array 
-// // I: Object 
-// // O: flattendArray
-// var flatten = function flatten(obj){
-// 	// create a mappedArr 
-// 	var mappedArr = [];
-// 	// create a recursive function
-// 	var recursive = function recursive(recursedObj){
-// 		//I: object
-// 		//O: push childrent into mappedArr
-// 		// Code:
-// 		// create a tempObj
-// 			// copy of Obj
-// 		var tempObj = Object.assign({},recursedObj);
-// 		// remove children
-// 		delete(tempObj.children);
-// 		// tempObj into mappedArr
-// 		mappedArr.push(tempObj);
+// Create a flatten function
+// Goal: takes children and maps them into an array 
+// I: Object 
+// O: flattendArray
+var flatten = function flatten(obj){
+	// create a mappedArr 
+	var mappedArr = [];
+	// create a recursive function
+	var recursive = function recursive(recursedObj){
+		//I: object
+		//O: push childrent into mappedArr
+		// Code:
+		// create a tempObj
+			// copy of Obj
+		var tempObj = Object.assign({},recursedObj);
+		// remove children
+		delete(tempObj.children);
+		// tempObj into mappedArr
+		mappedArr.push(tempObj);
 
-// 		// if obj.children === []
-// 		if(recursedObj.children.length < 1){
-// 			// return
-// 			return;
-// 		} else {
-// 			// else 
-// 			// iderate over obj.children
-// 			recursedObj.children.forEach((child)=>{
-// 				// recersive(obj.children[i])
-// 				recursive(child);
-// 			});
-// 		}
-// 	}
-// 	recursive(obj);
-// 	// return mappedArr
-// 	return mappedArr;
-// }
+		// if obj.children === []
+		if(recursedObj.children.length < 1){
+			// return
+			return;
+		} else {
+			// else 
+			// iderate over obj.children
+			recursedObj.children.forEach((child)=>{
+				// recersive(obj.children[i])
+				recursive(child);
+			});
+		}
+	}
+	recursive(obj);
+	// return mappedArr
+	return mappedArr;
+}
 
 
-// // Create a makeCSV function
-// 	// takes the flattenArray
-// 		// converts to a csv file 
-// 			// all colmn data except children
-// 			// all data in same order as original obj
-// 			// all comma dilimiated
-// var makeCSV = function makeCSV(flatArr){
-// 	var colmnNames = Object.getOwnPropertyNames(flatArr[0]);
-// 	var csv = [colmnNames];
-// 	flatArr.forEach((row)=>{
-// 	    var tempRow = [];
-// 	    colmnNames.forEach((colmn)=>{
-// 	        tempRow.push(row[colmn]);
-// 	    })
-// 	    csv.push(tempRow);
-// 	})
+// Create a makeCSV function
+	// takes the flattenArray
+		// converts to a csv file 
+			// all colmn data except children
+			// all data in same order as original obj
+			// all comma dilimiated
+var makeCSV = function makeCSV(flatArr){
+	var colmnNames = Object.getOwnPropertyNames(flatArr[0]);
+	var csv = [colmnNames];
+	flatArr.forEach((row)=>{
+	    var tempRow = [];
+	    colmnNames.forEach((colmn)=>{
+	        tempRow.push(row[colmn]);
+	    })
+	    csv.push(tempRow);
+	})
 
-// 	return csv.join('\n');
-// }
+	return csv.join('\n');
+}
 
-// // Call both functions
-// var flatArray = flatten(jsonObj);
-// var csvSend = makeCSV(flatArray);
+// Call both functions
+var flatArray = flatten(jsonObj);
+var csvSend = makeCSV(flatArray);
 
 
