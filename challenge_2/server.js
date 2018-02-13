af71c6a0aa7c4e472b2b4e4c34a52d8c33d3bfc1
EvaFro <frolli.erin@gmail.com>
2018-02-13 11:21:36 -0800

    Added a makeCSV function

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,8 +3,7 @@
 
 
 // Create a flatten function
-	// takes children and maps them into an array 
-
+// Goal: takes children and maps them into an array 
 // I: Object 
 // O: flattendArray
 var flatten = function flatten(obj){
@@ -42,13 +41,37 @@ var flatten = function flatten(obj){
 }
 
 
-
-
 // Create a makeCSV function
 	// takes the flattenArray
 		// converts to a csv file 
 			// all colmn data except children
 			// all data in same order as original obj
 			// all comma dilimiated
+var makeCSV = function makeCSV(flatArr){
+	var colmnNames = Object.getOwnPropertyNames(flatArr[0]);
+	var csv = [colmnNames];
+	flatArr.forEach((row)=>{
+	    var tempRow = [];
+	    colmnNames.forEach((colmn)=>{
+	        tempRow.push(row[colmn]);
+	    })
+	    csv.push(tempRow);
+	})
+
+	return csv.join('\n');
+}
+
+var flatArray = flatten(jsonObj);
+var csvSend = makeCSV(flatArray);
+
+
+
+
+
+
+
+
+
+
 
 
