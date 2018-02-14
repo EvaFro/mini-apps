50a8f38759909a152cbe375b823e17bb8b7c0302
EvaFro <frolli.erin@gmail.com>
2018-02-13 20:01:07 -0800

    Added proper post request

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,41 @@
+$(document).ready(function(){
+
+	$( "#convertForm" ).submit(function( event ) {
+	 
+	  // Stop form from submitting normally
+	  event.preventDefault();
+	 
+	  // Get some values from elements on the page:
+
+	  var $form = $( this );
+	  var jasonData = $form.find( "#jasonData" ).val();
+	    console.log("this is your data: ", typeof(jasonData))
+
+	    $.ajax({
+		  type: "POST",
+		  url: "/",
+		  contentType: "application/json",
+          dataType: "json",
+          data: jasonData,
+		  success: function(result){
+		  	console.log("result success: ",result)
+		  	$( "#csvFile" ).empty().append( result )
+		  },
+		  error: function(result) {console.log("result err: ", result)}
+		});
+
+
+	    //var posting = $.post('/', {data: JSON.stringify(jasonData)})
+
+	    // posting.done(function( data ) {
+	    // 	console.log("result data: ", data)
+	    // });
+	});
+
+
+});
+
+
+
 
 
