7ae8538d7c2755abd17b8f9062200dfd303a9c50
EvaFro <frolli.erin@gmail.com>
2018-02-13 22:31:35 -0800

    Deleted ConvertData.js because not needed

--- challenge_2/convertData.js
+++
@@ -1,8 +0,0 @@
-var csvFun = require(convertCSV);
-var convertData = function(request, response, next){
-	var flatData = csvFun.flatten(request.data);
-	var csvData = csvFun.makeCSV(flatData);
-	next();
-}
-
-exports.convertData = convertData;
