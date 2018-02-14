35670435c3bd7bddf65037bc88a06eda0545c2fc
EvaFro <frolli.erin@gmail.com>
2018-02-14 11:48:51 -0800

    Added basic requerments for express server

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,17 @@
 
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
+// parse data
+app.use(bodyParser.json());
+// load any static page from the client folder
+app.use(express.static(__dirname + '/client'));
+
+// serve up the index.html page
+app.get('/',(req, res)=>{
+	res.sendFile('/client/index.html');
+});
+
+// creat a listen even for a specific port
+app.listen(3000);
