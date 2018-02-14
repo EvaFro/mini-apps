d8bd2cb3fd58d700fe87467a7b440561d70985ff
EvaFro <frolli.erin@gmail.com>
2018-02-13 16:05:02 -0800

    Commented out a few things, tested func of loading static pages - passed

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,15 +1,25 @@
 var http = require("http");
-var convertData = require("convertData.js");
-var bodyParser = require('body-parser');
+// var convertData = require("convertData.js");
+//var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 
+// use my own middle where 
+// // may need to change this to only look for specific requests
+// app.use(convertData);
+
 // middle where that serves up any static files
-app.use(express.static('/client'))
+//app.use(express.static('/client'));
 
 // if the get request is "/" serve up html page
 app.get('/', (req, res)=>{
-	res.sendFile('/client/index.html')
-})
+	res.sendFile(__dirname +'/client/index.html')
+});
 
+// app.post((req, res)=>{
+
+// })
+
+
+// listen for requests on the server
 http.createServer(app).listen(3000);
