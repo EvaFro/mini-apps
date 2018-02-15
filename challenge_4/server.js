e0fa7d7323ead3c194d5a52cc542c6c6cfc73258
EvaFro <frolli.erin@gmail.com>
2018-02-15 10:35:44 -0800

    Set up an express server

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,15 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/client'));
+app.get('/', (req, res)=>{
+	res.sendFile(__dirname + '/client/index.html')
+})
+
+app.listen(3000);
 
 
