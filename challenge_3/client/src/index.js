a9e652883a148340ed7305a85c1ab8634b80d0ee
EvaFro <frolli.erin@gmail.com>
2018-02-14 21:37:25 -0800

    Added the initial render app via react

--- challenge_3/client/src/index.js
+++ challenge_3/client/src/index.js
@@ -1,1 +1,8 @@
-console.log('My Minimal React Webpack Babel Setup');
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './styles.css'
+
+ReactDOM.render(
+	<App />
+	document.getElementById('app')
+) 
