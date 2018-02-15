4f18880ed82f0413202e153900ecf8e89bf9be79
EvaFro <frolli.erin@gmail.com>
2018-02-15 07:46:53 -0800

    Added link to app - doesn't render need to fix how importing app.js

--- challenge_3/client/src/index.js
+++ challenge_3/client/src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '/app.js'
+import App from './app'
 // import './styles.css'
 
+
+const renderBlankBoard = function(numCol, numRows){
+	let board = [];
+	let i = numRows;
+	while(i>0){
+	  let arr = new Array(numCol).fill(null);
+	  board.push(arr);
+	  i--;
+	}
+	return board;
+}
+
+const blankBoard = renderBlankBoard(7,6);
+
 ReactDOM.render(
-	<App />,
-	document.getElementById('app')
-) 
+  <App board={blankBoard}/>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
