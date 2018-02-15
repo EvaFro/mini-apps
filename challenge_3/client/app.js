ba2bcf35943ac017ac06b895049eb8b9b5e4dfd1
EvaFro <frolli.erin@gmail.com>
2018-02-14 22:02:12 -0800

    Added basic outline for React App

--- challenge_3/client/app.js
+++ challenge_3/client/app.js
@@ -1,2 +1,57 @@
+inport React from 'react';
 
+class App extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			board: props.board,
+			curentPlayerRed: true;
+		}
+	}
+
+	render(){
+		return(
+			<div> 
+			<Board board{this.state.board} />
+
+			</div>
+
+		)
+	}
+}
+
+
+
+const Board = (props) =>(
+	<div className="board">
+	{props.board.map((row)=>{
+		<Row row{row} />
+	})}
+	</div>
+
+);
+
+
+const Row = (props)=>(
+	<div className="row">
+	{props.row.map((element, index)=>{
+		<Square piece={element} index={index} />
+	})}
+	</div>
+)
+
+const Square = (props) => (
+	<div className="square" onClick={()=>{
+		// function to place piece
+	}} >
+	{props.piece}
+	</div>
+)
+
+
+
+
+
+
+module.exports = { App };
 
