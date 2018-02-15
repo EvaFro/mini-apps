2517ed672849e7ee4988a10ec94d3981791d27c3
EvaFro <frolli.erin@gmail.com>
2018-02-14 22:03:06 -0800

    Added basic elemnts to render a react app

--- challenge_3/client/src/index.js
+++ challenge_3/client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from '/app.js'
 // import './styles.css'
 
 ReactDOM.render(
-	<App />
+	<App />,
 	document.getElementById('app')
 ) 
