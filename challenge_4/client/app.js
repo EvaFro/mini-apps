43bf11779979aeb44447b13878bf02556dca05f3
EvaFro <frolli.erin@gmail.com>
2018-02-15 19:52:55 -0800

    Added skeliton for react app

--- challenge_4/client/app.js
+++ challenge_4/client/app.js
@@ -1,2 +1,67 @@
+import React from 'react';
+
+class Game extends React.Component {
+	constructor(props){
+		super(props)
+		this.state{
+			this.pins = props.pins;
+			this.randomPins = [];
+			this.frame = 0;
+			this.isfirstRound = true;
+			this.numPins2PutDown = 0;
+
+		}
+	}
+
+	render() {
+		return(
+			<div className="game" >
+				<ScoreBoard />,
+				<PinArea />,
+				<NumberPad />
+			</div>
+		);
+	}
+}
+
+const ScoreBoard = (props) => {
+	<div className="scoreBoard" >
+	{}
+	</div>
+}
+
+const Frame = (props) => {
+	<div className='fullFrame' >
+	{}
+	</div>
+}
+
+const PinArea = (props) => {
+	<div className="pinArea" >
+	{}
+	</div>
+}
+
+const Pin = (props) => {
+	<div className="pin" >
+	{}
+	</div>
+}
+
+const NumberPad = (props) => {
+	<div className="numberPad" >
+	{}
+	</div>
+}
+
+
+const Keys = (props) => {
+	<div className="keys" >
+	{}
+	</div>
+}
+
+
+
 
 
